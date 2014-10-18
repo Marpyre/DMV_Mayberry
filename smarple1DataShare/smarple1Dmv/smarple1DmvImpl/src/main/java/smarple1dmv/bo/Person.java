175cd85b06ddf66ed3f2cb58c4a592f0b7499a7b
Seth Marple <smarple1@jhu.edu>
2014-10-18 13:00:22 -0400

    Adding Persistence

--- smarple1DataShare/smarple1Dmv/smarple1DmvImpl/src/main/java/smarple1dmv/bo/Person.java
+++ smarple1DataShare/smarple1Dmv/smarple1DmvImpl/src/main/java/smarple1dmv/bo/Person.java
@@ -1,8 +1,21 @@
 package smarple1dmv.bo;
 
 import java.util.Collection;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "smarple1dmv_person")
 public class Person {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String firstName;
 	private String middleName;
@@ -11,52 +24,67 @@ public class Person {
 	private Residence currentResidence;
 	private Collection<Residence> residences;
 	private PhysicalDetails physicalDetails;
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getMiddleName() {
 		return middleName;
 	}
+
 	public void setMiddleName(String middleName) {
 		this.middleName = middleName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getNameSuffix() {
 		return nameSuffix;
 	}
+
 	public void setNameSuffix(String nameSuffix) {
 		this.nameSuffix = nameSuffix;
 	}
+
 	public Residence getCurrentResidence() {
 		return currentResidence;
 	}
+
 	public void setCurrentResidence(Residence currentResidence) {
 		this.currentResidence = currentResidence;
 	}
+
 	public Collection<Residence> getResidences() {
 		return residences;
 	}
+
 	public void setResidences(Collection<Residence> residences) {
 		this.residences = residences;
 	}
+
 	public PhysicalDetails getPhysicalDetails() {
 		return physicalDetails;
 	}
+
 	public void setPhysicalDetails(PhysicalDetails physicalDetails) {
 		this.physicalDetails = physicalDetails;
 	}
